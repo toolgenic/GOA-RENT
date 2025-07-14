21f7386bcfcba43d1de3befd8d4a70c115acc5f5
toolgenic <toolgenic@gmail.com>
2025-07-14 21:57:42 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function scrollToListings() {
+  document.getElementById("listings").scrollIntoView({ behavior: "smooth" });
+}
+
+function payNow() {
+  alert("Redirecting to Razorpay... (Implement with Razorpay SDK)");
+}
